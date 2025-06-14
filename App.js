2858a4a48739af178ff69918f9995a6920adc73f
Manisha-kakade <manishakakade95@gmail.com>
2025-06-14 19:47:24 +0530

    initial commit

---
+++ App.js
@@ -0,0 +1,18 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+
+const App = () => {
+ 
+
+  return (
+    <div>
+      <h1> THIS is App</h1>
+    </div>
+  );
+};
+
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <App />
+);
